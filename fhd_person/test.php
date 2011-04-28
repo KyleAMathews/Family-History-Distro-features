7292f550126c0fc19dc0ad361525a33010503b53
Kyle Mathews <mathews.kyle@gmail.com>
2011-04-28 13:20:27 -0700

    Sample code for parsing gedcom file and creating entities from them.

--- fhd_person/test.php
+++ fhd_person/test.php
@@ -1,6 +1,27 @@
 <?php
 
-//print_r(entity_get_info('fhd_person'));
+require_once 'Genealogy/Gedcom.php';
+
+$ged = new Genealogy_Gedcom('sites/all/modules/familyhistorydistro-features/fhd_person/smallGed55Format.ged');
+$persons = $ged->GedcomIndividualsTreeObjects;
+
+foreach ($persons as $person) {
+  $bplace = $person->Birth['Place'];
+  $entity = array(
+    'name' => fixEncoding($person->Firstname . $person->Lastname),
+    'birth_date' => strtotime($person->Birth['Date']),
+    'birth_place' => utf8_encode($person->Birth['Place']),
+    'family_search_id' => $person->Identifier,
+  );
+  entity_save('fhd_person', $entity);
+  mb_detect_encoding($bplace, "UTF-8") == "UTF-8" ? '' : $bplace = utf8_encode($bplace);
+  $bplace = utf8_encode($bplace);
+  echo "encoding: " . mb_detect_encoding($bplace) . "\n";
+  echo "name: " . $person->Firstname . $person->Lastname . "\n";
+  echo "birth place: " . $bplace . "\n";
+  echo "birth date: " . strtotime($person->Birth['Date']) . "\n";
+  echo "\n";
+}
 
 $entity = array(
   'name' => "Fred Flinstone",
@@ -11,6 +32,8 @@ $entity = array(
 //print_r(entity_save('fhd_person', $entity));
 
 
+/** Loading **/
+/*
 $entities = entity_load('fhd_person', array(7));
 //print_r($entities[1]);
 $entity = $entities[1];
@@ -20,4 +43,14 @@ echo "\nentity uri:\n";
 echo "\nentity identifier: " . $entity->identifier() . "\n";
 echo "\nentity url: " . $entity->url() . "\n";
 echo "\nentity path: " . $entity->path() . "\n";
+ */
 
+// Fixes the encoding to uf8 
+function fixEncoding($in_str) 
+{ 
+  $cur_encoding = mb_detect_encoding($in_str) ; 
+  if($cur_encoding == "UTF-8" && mb_check_encoding($in_str,"UTF-8")) 
+    return $in_str; 
+  else 
+    return utf8_encode($in_str); 
+} // fixEncoding 
