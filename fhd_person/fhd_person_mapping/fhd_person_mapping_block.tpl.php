b7933bd73329fafbcbaaf9f5b34a6ec2b9af46ce
Kyle Mathews <mathews.kyle@gmail.com>
2011-05-05 11:42:00 -0700

    Moved logic from template file into preprocessor function.

--- fhd_person/fhd_person_mapping/fhd_person_mapping_block.tpl.php
+++ fhd_person/fhd_person_mapping/fhd_person_mapping_block.tpl.php
@@ -1,5 +1,5 @@
 <div class="associated-person">
-  <span class="name"><?php print l($person->name, 'person/' . $person->pid); ?></span> <br />
-  <strong>Born:</strong> <?php print date('Y', $person->birth_date) . ' ' . check_plain($person->birth_place); ?>
+  <span class="name"><?php print $person_link; ?></span> <br />
+  <strong>Born:</strong> <?php print $birth_string; ?>
   <span data-nid="<?php print $nid; ?>" data-pid="<?php print $person->pid; ?>" class="remove-referenced-person" title="Remove this referenced person">x<span>
 </div>
