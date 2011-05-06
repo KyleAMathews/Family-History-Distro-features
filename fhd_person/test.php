8c0a613cdca808e46155faf151f03d4eb9d63849
Kyle Mathews <mathews.kyle@gmail.com>
2011-05-06 10:31:05 -0700

    Added changes to the test file.

--- fhd_person/test.php
+++ fhd_person/test.php
@@ -2,15 +2,15 @@
 
 require_once 'Genealogy/Gedcom.php';
 
-$ged = new Genealogy_Gedcom('sites/all/modules/familyhistorydistro-features/fhd_person/smallGed55Format.ged');
+$ged = new Genealogy_Gedcom('/tmp/sevenGenerationsLegacy.ged');
 $persons = $ged->GedcomIndividualsTreeObjects;
 
 foreach ($persons as $person) {
 
   $bplace = $person->Birth['Place'];
   $entity = array(
-    'name' => fixEncoding($person->Firstname . " " . $person->Lastname),
-    'birth_date' => strtotime($person->Birth['Date']),
+    'name' => fixEncoding(trim($person->Firstname) . " " . trim($person->Lastname)),
+    'birth_date' => _fhd_get_date($person->Birth['Date']),
     'birth_place' => utf8_encode($person->Birth['Place']),
     'sex' => $person->Sex,
     'gedcom_id' => $person->Identifier,
@@ -18,22 +18,25 @@ foreach ($persons as $person) {
 
     // Gedcom parser erroneously duplicates birth info into death/burial
     if ($person->Death['Date'] != $person->Birth['Date']) {
-      $entity['death_date'] = strtotime($person->Death['Date']);
+      $entity['death_date'] = _fhd_get_date($person->Death['Date']);
       $entity['death_place'] = utf8_encode($person->Death['Place']);
     }
     if ($person->Burial['Date'] != $person->Birth['Date']) {
-      $entity['burial_date'] = strtotime($person->Burial['Date']);
+      $entity['burial_date'] = _fhd_get_date($person->Burial['Date']);
       $entity['burial_place'] = utf8_encode($person->Burial['Place']);
     }
-  print_r($entity);
-  entity_save('fhd_person', $entity);
+    if($entity['birth_date'] == 0) {
+  //   print_r($person->Birth);
+//  print_r($entity);
+    }
+//  entity_save('fhd_person', $entity);
   mb_detect_encoding($bplace, "UTF-8") == "UTF-8" ? '' : $bplace = utf8_encode($bplace);
   $bplace = utf8_encode($bplace);
-  echo "encoding: " . mb_detect_encoding($bplace) . "\n";
+/*  echo "encoding: " . mb_detect_encoding($bplace) . "\n";
   echo "name: " . $person->Firstname . $person->Lastname . "\n";
   echo "birth place: " . $bplace . "\n";
   echo "birth date: " . strtotime($person->Birth['Date']) . "\n";
-  echo "\n";
+  echo "\n"; */
 }
 
 $entity = array(
@@ -67,3 +70,14 @@ function fixEncoding($in_str)
   else 
     return utf8_encode($in_str); 
 } // fixEncoding 
+
+function _fhd_get_date($str) {
+  if (($timestamp = strtotime($str)) === false && !empty($str)) {
+    // Trim off "Abt" or other odd charecters that slip in
+    $str = trim($str, ' Aabt<>');
+    // Sometimes there's a date range, just grab the first.
+    $str = array_pop(explode('-',$str));
+    $timestamp = strtotime($str);
+  }
+  return $timestamp;
+}
