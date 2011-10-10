28a0aec493c45920b0dd344d10b8c9ad6797f55a
Kyle Mathews <mathews.kyle@gmail.com>
2011-10-09 21:19:51 -0700

    Added import script.

---
+++ import_gedcom.php
@@ -0,0 +1,8 @@
+<?php
+
+# Add the path to your gedcom file.
+# Then run this script from the command line by running "drush scr import_gedcom.php".
+
+$file_path = '';
+
+fhd_person_load_gedcom($file_path);
