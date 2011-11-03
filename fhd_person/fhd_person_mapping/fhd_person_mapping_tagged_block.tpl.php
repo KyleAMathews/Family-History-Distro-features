2053130cebbc33c63b475c16b3f6c27d9a54a051
Kyle Mathews <mathews.kyle@gmail.com>
2011-11-02 23:41:55 -0700

    Person pages now include summaries of all content associated with that person for easier scanning.

--- fhd_person/fhd_person_mapping/fhd_person_mapping_tagged_block.tpl.php
+++ fhd_person/fhd_person_mapping/fhd_person_mapping_tagged_block.tpl.php
@@ -1,10 +1,21 @@
 <div class="block">
   <h2><?php print fhd_core_type_pluralizer($nodes[0]->type); ?></h2>
-  <ul>
 <?php
+$count = 1;
+$length = count($nodes);
 foreach($nodes as $node) {
-  print "<li>" . l($node->title, 'node/' . $node->nid) . "</li>";
+  print "<article class='tagged-content'>";
+  print  "<h1>" . l($node->title, 'node/' . $node->nid) . "</h1>";
+  print text_summary($node->body['und'][0]['safe_value'], NULL, 1200);
+  print  "<span class='read-more'>" .
+    l(t('Read more') . " &#8594;", 'node/' . $node->nid, array('html' => TRUE)) .
+    "</span>";
+  print "</article>";
+  // Add a dividing line between rows unless we're at the end.
+  if ($count % 2 === 0 && $length - $count !== 0) {
+    print "<div class='clear-both'></div>";
+  }
+  $count++;
 }
 ?>
-  </ul>
 </div>
